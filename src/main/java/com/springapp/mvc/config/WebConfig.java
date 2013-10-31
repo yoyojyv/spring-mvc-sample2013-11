9a598d65e61384063f375d2d39b054ee27698335
yoyojyv <yoyojyv@gmail.com>
2013-11-01 00:32:28 +0900

    first commit

--- src/main/java/com/springapp/mvc/config/WebConfig.java
+++ src/main/java/com/springapp/mvc/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.LocaleResolver;
@@ -29,48 +28,48 @@ public class WebConfig extends WebMvcConfigurerAdapter {
   /**
    * CSS / JavaScript / Image 등의 정적 리소스를 처리해주는 핸들러를 등록
    */
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+  }
 
   /**
    * 인터셉터 (요청을 가로챔)
    */
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
 
-		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-		localeChangeInterceptor.setParamName("lang");
-		registry.addInterceptor(localeChangeInterceptor);
-	}
+    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    localeChangeInterceptor.setParamName("lang");
+    registry.addInterceptor(localeChangeInterceptor);
+  }
 
   /**
    * locale resolver
    *
    * @return
    */
-	@Bean
-	public LocaleResolver localeResolver() {
+  @Bean
+  public LocaleResolver localeResolver() {
 
-		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
-		cookieLocaleResolver.setDefaultLocale(StringUtils.parseLocaleString("en"));
-		return cookieLocaleResolver;
-	}
+    CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+    cookieLocaleResolver.setDefaultLocale(StringUtils.parseLocaleString("en"));
+    return cookieLocaleResolver;
+  }
 
   /**
    * JSP를 뷰로 사용하는 뷰 리졸버 등록
    */
-	@Bean
-	public ViewResolver viewResolver() {
+  @Bean
+  public ViewResolver viewResolver() {
 
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setViewClass(JstlView.class);
+    viewResolver.setPrefix("/WEB-INF/views/");
+    viewResolver.setSuffix(".jsp");
 
-		return viewResolver;
-	}
+    return viewResolver;
+  }
 
   @Override
   public void addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry registry) {
@@ -89,19 +88,19 @@ public class WebConfig extends WebMvcConfigurerAdapter {
    *
    * @return
    */
-	@Bean
-	public MessageSource messageSource() {
+  @Bean
+  public MessageSource messageSource() {
 
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasenames("classpath:messages/messages", "classpath:messages/validation");
-		// if true, the key of the message will be displayed if the key is not
-		// found, instead of throwing a NoSuchMessageException
-		messageSource.setUseCodeAsDefaultMessage(true);
-		messageSource.setDefaultEncoding("UTF-8");
-		// # -1 : never reload, 0 always reload
-		messageSource.setCacheSeconds(0);
-		return messageSource;
-	}
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasenames("classpath:messages/messages", "classpath:messages/validation");
+    // if true, the key of the message will be displayed if the key is not
+    // found, instead of throwing a NoSuchMessageException
+    messageSource.setUseCodeAsDefaultMessage(true);
+    messageSource.setDefaultEncoding("UTF-8");
+    // # -1 : never reload, 0 always reload
+    messageSource.setCacheSeconds(0);
+    return messageSource;
+  }
 
 //  /**
 //   * {@link org.springframework.core.env.Environment}를 사용해 빈 주입 값을 치환.

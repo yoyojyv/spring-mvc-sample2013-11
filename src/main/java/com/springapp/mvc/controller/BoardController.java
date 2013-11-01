9f85fe8d797558ac1430ee7d1c793f9af4e0421b
yoyojyv <yoyojyv@gmail.com>
2013-11-01 10:22:57 +0900

    03 상세뷰, 폼 페이지 관련 컨트롤러 작성

--- src/main/java/com/springapp/mvc/controller/BoardController.java
+++ src/main/java/com/springapp/mvc/controller/BoardController.java
@@ -1,10 +1,17 @@
 package com.springapp.mvc.controller;
 
+import com.springapp.mvc.domain.Board;
 import com.springapp.mvc.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,13 +26,32 @@ public class BoardController {
   @Autowired
   private BoardService boardService;
 
-
   @RequestMapping("/board/list")
   public String list(Model model) {
     model.addAttribute("boardList", boardService.findAll());
     return "board/list";
   }
 
+  @RequestMapping("/board/show/{id}")
+  public String view(@PathVariable Long id, Model model) {
+    model.addAttribute("board", boardService.findById(id));
+    return "board/view";
+  }
 
+  @RequestMapping("/board/edit/{id}")
+  public String edit(@PathVariable Long id, Model model) {
+    model.addAttribute("board", boardService.findById(id));
+    return "board/edit";
+  }
+
+  @RequestMapping("/board/create")
+  public String create(Model model) {
+    return "board/create";
+  }
+
+  @RequestMapping(value = "/board/save", method = RequestMethod.POST)
+  public String save(@Valid Board board, BindingResult bindingResult) {
+    return "redirect:/board/list";
+  }
 
 }
